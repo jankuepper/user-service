b6aaaa4476c3ee4723c05501c58d25aca6b37963
Jan <janluca0000046@gmail.com>
2025-02-07 20:03:14 +0100

    fix: token expiration

--- internal/services/jwt.go
+++ internal/services/jwt.go
@@ -13,7 +13,7 @@ func CreateToken(email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
 		jwt.MapClaims{
 			"email": email,
-			"exp":   time.Now().Add(time.Hour * 24).Unix(),
+			"exp":   time.Now().Add(time.Minute * 10).Unix(),
 		})
 
 	tokenString, err := token.SignedString(secretKey)

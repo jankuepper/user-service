e5248fc2c6a9a038db5bcf1f96a5da70cf9a42c3
jankuepper <janluca0000046@gmail.com>
2025-02-08 19:01:43 +0100

    cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -11,11 +11,11 @@ func Cors(next http.Handler) http.Handler {
 		// https://stackoverflow.com/questions/47298310/golang-http-allow-certain-domain-name-both-with-www-and-without
 		fmt.Println("origin", r.Header.Get("Origin"))
 		switch origin := r.Header.Get("Origin"); origin {
-		case "www.jankuepper.de", "jankuepper.de":
+		case "www.jankuepper.de", "jankuepper.de", "https://www.jankuepper.de", "https://jankuepper.de":
 			w.Header().Add("Access-Control-Allow-Origin", origin)
-		case "www.jankuepper.eu", "jankuepper.eu":
+		case "www.jankuepper.eu", "jankuepper.eu", "https://www.jankuepper.eu", "https://jankuepper.eu":
 			w.Header().Add("Access-Control-Allow-Origin", origin)
-		case "localhost:5173", "localhost:4173":
+		case "localhost:5173", "http://localhost:5173", "localhost:4173", "http://localhost:4173":
 			w.Header().Add("Access-Control-Allow-Origin", origin)
 		default:
 			w.Header().Add("Access-Control-Allow-Origin", "www.jankuepper.eu")

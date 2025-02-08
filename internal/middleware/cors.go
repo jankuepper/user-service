356f02aecc19d10b3a3d91d641418717f296bc96
jankuepper <janluca0000046@gmail.com>
2025-02-08 19:03:35 +0100

    cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -12,17 +12,17 @@ func Cors(next http.Handler) http.Handler {
 		fmt.Println("origin", r.Header.Get("Origin"))
 		switch origin := r.Header.Get("Origin"); origin {
 		case "www.jankuepper.de", "jankuepper.de", "https://www.jankuepper.de", "https://jankuepper.de":
-			w.Header().Add("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 		case "www.jankuepper.eu", "jankuepper.eu", "https://www.jankuepper.eu", "https://jankuepper.eu":
-			w.Header().Add("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 		case "localhost:5173", "http://localhost:5173", "localhost:4173", "http://localhost:4173":
-			w.Header().Add("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 		default:
-			w.Header().Add("Access-Control-Allow-Origin", "www.jankuepper.eu")
+			w.Header().Set("Access-Control-Allow-Origin", "www.jankuepper.eu")
 		}
-		w.Header().Add("Access-Control-Allow-Credentials", "true")
-		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		if r.Method == "OPTIONS" {
 			http.Error(w, "No Content", http.StatusNoContent)
 			return
